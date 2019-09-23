a125eb44bbb66abfe8f63c832b7d15010647b214
Ian Manners <34320092+IanManners1997@users.noreply.github.com>
2019-09-23 16:59:00 -0400

    Add files via upload

---
+++ codeskeleton.java
@@ -0,0 +1,93 @@
+/**
+ * Phase 2 Submission
+ */
+
+import java.io.*;
+import java.sql.*;
+import java.text.ParseException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class MyDB{
+
+
+	public static void main(String args[]) {
+
+	
+
+	}
+
+	static void initForTesting() {
+		
+	}
+
+	static void loginProfessor() {
+		
+	}
+
+	private static void loginStudent() {
+		
+	}
+
+	private static void displayAdminMenu() {
+		
+	}
+
+	static void addStudent() {
+		
+
+	}
+
+	static void updateStudent() {
+		
+	}
+
+	static void StudentStatistics() {
+		
+	}
+
+	static void CourseStatistics() {
+	
+	}
+
+	static void browseCourses() {
+		
+
+	}
+
+	static void searchCourses() {
+		
+	}
+
+	static void AddACourse() {
+		
+	}
+
+	static void GradeACourse() throws Exception{
+		
+}
+
+	
+
+
+	static void getSuggestion() throws Exception{
+		
+	}
+
+	static void highestVolumeNoSub() {
+		
+			}
+
+	static void highestVolumeNoCourse() {
+		
+	}
+
+	static void mostActiveProfessor() {
+ 		
+ 	}
+
+ 	static void mostActiveStudent() {
+		
+		}
+
+}
---
+++ dbskeleton.sql
@@ -0,0 +1,25 @@
+
+drop table student cascade constraints;
+
+
+create table student(
+
+);
+
+
+drop table professor cascade constraints;
+create table professor(
+
+);
+
+
+drop table grader cascade constraints;
+create table grader(
+
+);
+ 
+drop table  course constraints;
+create table course(
+
+);
+
