3b1dc585c3ca1e542548d428e6faa583473902ed
ayoo324 <adi11@pitt.edu>
2019-10-01 11:54:08 -0400

    Add files via upload

---
+++ testdb/testdb.sql
@@ -0,0 +1,197 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 01, 2019 at 05:53 PM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `testdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `graders`
+--
+
+CREATE TABLE `graders` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `data` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `graders`
+--
+
+INSERT INTO `graders` (`id`, `name`, `data`) VALUES
+(0, 'testgrader', 'test'),
+(1, 'test2222', 'asdvfadsva'),
+(2, 'TESTINSERT', 'TESTDATA');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `instructors`
+--
+
+CREATE TABLE `instructors` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `data` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `instructors`
+--
+
+INSERT INTO `instructors` (`id`, `name`, `data`) VALUES
+(0, 'Hoffman', 'test'),
+(1, 'TESTINSERT', 'TESTDATA');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `relations`
+--
+
+CREATE TABLE `relations` (
+  `section_id` int(11) NOT NULL,
+  `grader_id` int(11) NOT NULL,
+  `instructor_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `relations`
+--
+
+INSERT INTO `relations` (`section_id`, `grader_id`, `instructor_id`) VALUES
+(1, 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sections`
+--
+
+CREATE TABLE `sections` (
+  `id` int(11) NOT NULL,
+  `data` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sections`
+--
+
+INSERT INTO `sections` (`id`, `data`) VALUES
+(0, 'testtttt'),
+(1, 'TESTDATA');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `testrel`
+--
+
+CREATE TABLE `testrel` (
+  `id1` int(11) NOT NULL,
+  `id2` int(11) NOT NULL,
+  `id3` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `graders`
+--
+ALTER TABLE `graders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `instructors`
+--
+ALTER TABLE `instructors`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `relations`
+--
+ALTER TABLE `relations`
+  ADD UNIQUE KEY `section_id` (`section_id`),
+  ADD KEY `Grader id` (`grader_id`),
+  ADD KEY `Instructor id` (`instructor_id`);
+
+--
+-- Indexes for table `sections`
+--
+ALTER TABLE `sections`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `testrel`
+--
+ALTER TABLE `testrel`
+  ADD KEY `grader` (`id2`),
+  ADD KEY `instructor` (`id3`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `graders`
+--
+ALTER TABLE `graders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `instructors`
+--
+ALTER TABLE `instructors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `sections`
+--
+ALTER TABLE `sections`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `relations`
+--
+ALTER TABLE `relations`
+  ADD CONSTRAINT `Grader id` FOREIGN KEY (`grader_id`) REFERENCES `graders` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `Instructor id` FOREIGN KEY (`instructor_id`) REFERENCES `instructors` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `section id` FOREIGN KEY (`section_id`) REFERENCES `sections` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `testrel`
+--
+ALTER TABLE `testrel`
+  ADD CONSTRAINT `grader` FOREIGN KEY (`id2`) REFERENCES `graders` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `instructor` FOREIGN KEY (`id3`) REFERENCES `instructors` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
