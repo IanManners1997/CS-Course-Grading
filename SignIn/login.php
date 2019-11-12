5d67936c9fa2ab9ed03b01b382b270a9a95c94c6
Ian Manners <34320092+IanManners1997@users.noreply.github.com>
2019-11-12 13:36:22 -0500

    Add files via upload

--- SignIn/login.php
+++ SignIn/login.php
@@ -1,23 +1,44 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Login Page</title>
-	<link rel = "stylesheet" type = "text/css" href="style.css">
+	<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
+	<link type="text/css" href="css/style.css" rel="stylesheet" />
+	
+	<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+	<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+
+	<script src="js/tools.js" type="text/javascript"></script>
+	<script src='js/jquery.form.js' type='text/javascript'></script>
+	
+	
+	<title>Professor Hoffman's CS 445</title>
 </head>
 <body>
-	<div id = "frm">
-		<form action = "process.php" method="POST">
-			<p>
-				<label>Username:</label>
-				<input type = "text" id ="user" name = "user" />
-			</p>
-			<p>
-				<label>Password:</label>
-				<input type = "password" id ="pass" name = "pass" />
-			</p>
-		</form>
+	<div id="header">
+		<a id='title' href='index.php'>Professor Hoffman's CS 445</a>
+			</div>
+					<div id="login">
+					<p>
+						Authentication Required<br />
+						<span class="error"></span>
+					</p>
+					<form action="index.php" method="post">
+						<table>
+							<tr>
+								<td>ID: (jfk63 lower case)<br></td>
+								<td><input name="uid" type="text" /></td>
+							</tr>
 			
-	</div>
-
+							<tr>
+								<td>pplSoft#: (9182734)<br></td>
+								<td><input name="unum" type="password" /></td>
+							</tr>
+						</table>
+						<input type="submit" value="Login"/>
+					</form>
+				</div>
+						<div id="footer">
+			Send all bug reports to <a href="mailto:hoffmant@pitt.edu">hoffmant@cs.pitt.edu</a>
+		</div>
 </body>
 </html>
