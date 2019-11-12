8ae99fb6c5c842b7f4ddddc0b0f0a9b4de743002
Ian Manners <34320092+IanManners1997@users.noreply.github.com>
2019-11-12 13:28:59 -0500

    Add files via upload

--- SignIn/login.php
+++ SignIn/login.php
@@ -9,8 +9,14 @@
 		<form action = "process.php" method="POST">
 			<p>
 				<label>Username:</label>
+				<input type = "text" id ="user" name = "user" />
+			</p>
+			<p>
+				<label>Password:</label>
+				<input type = "password" id ="pass" name = "pass" />
 			</p>
 		</form>
+			
 	</div>
 
 </body>
